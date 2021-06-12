e86bd8e56be9172295e248f6594562b73ca0b8b3
zeon77 <zeon77@gmail.com>
2021-06-12 21:29:38 +0200

    Add project files.

---
+++ Versenyzők/Pilóta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Versenyzők
+{
+    class Pilóta
+    {
+        public string Név { get; set; }
+        public DateTime SzületésiDátum { get; set; }
+        public string Nemzetiség { get; set; }
+        public int? Rajtszám { get; set; }
+
+        public Pilóta(string sor)
+        {
+            string[] s = sor.Split(';');
+            Név = s[0];
+            SzületésiDátum = DateTime.Parse(s[1]);
+            Nemzetiség = s[2];
+            Rajtszám = s[3] == "" ? null : int.Parse(s[3]);
+        }
+    }
+}
